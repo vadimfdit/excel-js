9a7eeccd7a150d517ae8ba93bcfe0f097d1617aa
vadim kovalenko <v.kovalenko@sendpulse.com>
2021-02-03 17:31:08 +0200

    minor changes

--- src/core/Emitter.js
+++ src/core/Emitter.js
@@ -29,21 +29,3 @@ export class Emitter {
     }
   }
 }
-
-// Example
-// const emitter = new Emitter()
-//
-// const unsub = emitter.subscribe('vladilen', data => console.log(data))
-// emitter.emit('1231231', 42)
-//
-// setTimeout(() => {
-//   emitter.emit('vladilen', 'After 2 seconds')
-// }, 2000)
-//
-// setTimeout(() => {
-//   unsub()
-// }, 3000)
-//
-// setTimeout(() => {
-//   emitter.emit('vladilen', 'After 4 seconds')
-// }, 4000)
